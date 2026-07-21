132c31207ab667e5e13d966b617143b9e59d43f4
Anand dev <116546462+dev4anand@users.noreply.github.com>
2026-07-21 11:41:54 +0530

    Add files via upload

---
+++ rk/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('#exampleModal').modal('show');
+
+    // Hide the modal after 5 seconds (5000 milliseconds)
+    // setTimeout(function() {
+      $('#exampleModal').modal('hide');
+    // }, 2000);
+  });
+
+
+
+//  gmail:	rkwedding111@gmail.com	name:RK WEDDING MALL VITHURA	place:VITHURA	number:9961680000	facebook:			https://www.facebook.com/share/1Qtz3nQwtX/?mibextid=wwXIfr,	instagram :https://www.instagram.com/rk_wedding_mall_vithura?igsh=Z3BkM3JzcmdobGRw&utm_source=qr youtube:	https://youtube.com/@rkweddingmallvithura?si=TE6xaIu91-s-JvoA googlemap:	https://www.google.com/maps/place/R+K+Wedding+Mall,+Vithura/@8.6702257,77.0816999,17z/data=!3m2!4b1!5s0x3b05c96df6ce1bf7:0x40728c7175c78b6b!4m6!3m5!1s0x3b05c96df6c1e8a5:0xb9e3d67419da6ee6!8m2!3d8.6702204!4d77.0842748!16s%2Fg%2F11hbqq9b9x?entry=ttu&g_ep=EgoyMDI2MDcxNS4wIKXMDSoASAFQAw%3D%3D	whatsapp no:9961680000			CLOTHING AND APPARELS/HOME APPLAINCES/ FOOTWEAR AND BAGS/ FANCY AND COSMETICS/ PERFUMES/ BRANDED WATCHES/	"VITHURA
+// KALUNK JUNCTION"		https://drive.google.com/open?id=17ZRUExdSVwa3uqIQUr-TeXguIistLihN								
